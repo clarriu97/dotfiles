44bd7a5215614368937299eca484cf87d44899dd
Carlos Larriu <carlos.larriu.intern@veridas.com>
2022-09-29 18:34:42 +0200

    screenlayout files to be used with xrandr

---
+++ i3/.screenlayout/home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --primary --mode 2560x1080 --pos 0x797 --rotate normal --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate right --output HDMI-2 --off
---
+++ i3/.screenlayout/mirror.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-1 --same-as eDP-1
