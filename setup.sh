3b1177a80bf8728e826cf3e25a6ccf94aed18e38
Carlos Larriu <larriucarlos@gmail.com>
2023-09-24 16:20:21 +0200

    added vlc installation for Fedora

--- setup.sh
+++ setup.sh
@@ -192,6 +192,12 @@ function install_fedora_dependencies {
     sudo dnf install -y polybar && \
     echo -e "${green}polybar${cyan} installed!${nc}\n" && \
 
+    echo -e "Installing ${orange}vlc${cyan}...${nc}" && \
+    sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm && \
+    sudo dnf install -y 'https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-38.noarch.rpm' && \
+    sudo dnf install -y vlc && \
+    echo -e "${green}vlc${cyan} installed!${nc}\n" && \
+
     echo -e "Installing ${orange}ffmpeg${cyan}...${nc}" && \
     sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm && \
     sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm && \
