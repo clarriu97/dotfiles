371af79d522bb519f36ee23ec669755ab470c1b1
Carlos Larriu <larriucarlos@gmail.com>
2024-09-01 12:15:32 +0200

    updated home screenlayout script

--- i3/.screenlayout/home.sh
+++ i3/.screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --primary --mode 2560x1080 --pos 0x797 --rotate normal --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate right --output HDMI-2 --off
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2-3-1 --primary --mode 3440x1440 --refresh 120 --output HDMI-2 --off
